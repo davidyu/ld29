5ab2af0ae6a3a301506bd81026045fc022c068af
David Yu <lewen.yu@gmail.com>
2014-04-26 15:16:10 -0700

    move player (seems to move as expected within circle)

--- entry/Assets/PlayerController.cs
+++ entry/Assets/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public Vector2 moveSpeed = new Vector2( 0.4f, 0.4f );
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +11,16 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	}
+
+	void FixedUpdate() {
+		float h = Input.GetAxis( "Horizontal" );
+		if( h < 0 ) {
+			rigidbody2D.velocity = new Vector2( -moveSpeed.x, 0 );
+		}
+
+		if ( h > 0 ) {
+			rigidbody2D.velocity = new Vector2( moveSpeed.x, 0 );
+		}
 	}
 }
