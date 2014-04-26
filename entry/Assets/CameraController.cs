eb71c902ea10d062b157ff8e0f16bcf17958e9b4
David Yu <lewen.yu@gmail.com>
2014-04-26 16:37:27 -0700

    start working on follow camera

---
+++ entry/Assets/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject target = null;
+
+	// Use this for initialization
+	void Start () {
+		// acquire player
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// get entity with tag (player)
+		if ( target != null ) {
+			this.transform.position = target.transform.position;
+		}
+	}
+}
