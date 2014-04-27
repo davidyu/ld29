bd30352afb074a7be16625a833f354cf580fba5b
David Yu <lewen.yu@gmail.com>
2014-04-26 20:34:30 -0700

    fix follow camera

--- entry/Assets/CameraController.cs
+++ entry/Assets/CameraController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public GameObject target = null;
+	public float cameraZ = -10.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,9 +13,9 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// get entity with tag (player)
 		if ( target != null ) {
-			this.transform.position = target.transform.position;
+			this.transform.position = new Vector3( target.transform.position.x, target.transform.position.y, cameraZ );
+			this.transform.rotation = target.transform.rotation;
 		}
 	}
 }
