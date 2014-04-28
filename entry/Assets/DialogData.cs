97c0660775150a77f2264b71ac9c599ee176917f
David Yu <lewen.yu@gmail.com>
2014-04-28 01:27:54 -0400

    rotating dialog system

--- entry/Assets/DialogData.cs
+++ entry/Assets/DialogData.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 public class DialogData : MonoBehaviour {
 
 	public List<string> lines; //unused for now
+	public int lineSwapInterval = 3;
 
 	// Use this for initialization
 	void Start () {
--- entry/Assets/RoseDialogData.cs
+++ entry/Assets/RoseDialogData.cs
@@ -10,16 +10,42 @@ public enum RoseState {
 }
 
 [Serializable]
-public class LineState {
+public class StateLineData {
 	public List<string> lines;
 	public RoseState state;
 }
 
+[Serializable]
+public class DialogState {
+	public int lineIndex;
+	public RoseState state;
+
+	public DialogState( int sli, RoseState ss ) {
+		lineIndex = sli;
+		state = ss;
+	}
+}
+
 public class RoseDialogData : DialogData {
-	public RoseState currentState = RoseState.REQUEST_HELP;
-	public List<LineState> roseLines;
+	public DialogState currentState = new DialogState( 0, RoseState.REQUEST_HELP );
+	public List<StateLineData> roseLines;
+
+	private DateTime lastUpdated;
+
+	void Start() {
+		lastUpdated = DateTime.Now;
+	}
 
 	// Update is called once per frame
 	void Update () {
+		for ( int i = 0; i < Enum.GetNames( typeof( RoseState ) ).Length; i++ ) {
+			if ( (RoseState) i == currentState.state && (DateTime.Now - lastUpdated).Seconds >= lineSwapInterval ) {
+				currentState.lineIndex = ( currentState.lineIndex + 1 ) % roseLines[i].lines.Count;
+				lastUpdated = DateTime.Now;
+			}
+		}
+
+
+		GetComponent<TextMesh>().text = roseLines[ (int) currentState.state ].lines[ currentState.lineIndex ];
 	}
 }
