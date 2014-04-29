57c23a4c5685481fd7cdfc98aa99b5b3215cae43
David Yu <lewen.yu@gmail.com>
2014-04-28 23:44:12 -0700

    bomber turns magnet on and off every 2 seconds

--- entry/Assets/BomberController.cs
+++ entry/Assets/BomberController.cs
@@ -10,13 +10,23 @@ public class BomberController : MonoBehaviour {
 	public float drot; // rotation velocity in radians
 	public float bomberMagnetMultiplier = 5f; // bomber's magnet is 5x powerful than player's
 	public bool magnetIsActive = false;
+	public int magnetDuration = 2;
+	private DateTime lastUpdated;
 
 	// Use this for initialization
 	void Start () {
+		lastUpdated = DateTime.Now;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// update magnet status
+		if ( (DateTime.Now - lastUpdated).Seconds >= magnetDuration ) {
+			magnetIsActive = !magnetIsActive;
+			lastUpdated = DateTime.Now;
+		}
+
+		// update rotation
 		rotation += drot;
 		// normalize
 		if ( rotation > 2 * Math.PI ) {
