f463121763c93a85062fe6feb02df34362726806
David Yu <lewen.yu@gmail.com>
2014-04-28 23:10:55 -0700

    first pass bomber

---
+++ entry/Assets/BomberController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BomberController : MonoBehaviour {
+
+	public GameObject center;
+	public float orbitRadius;
+	public float rotation; // in radians
+	public float drot; // rotation velocity in radians
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotation += drot;
+		// normalize
+		if ( rotation > 2 * Math.PI ) {
+			rotation = 0;
+		}
+
+		if ( center == null ) return;
+
+		Vector2 c = new Vector2( center.transform.position.x, center.transform.position.y );
+		transform.position = new Vector3( c.x + (float) Math.Cos( rotation ) * orbitRadius, c.y - (float) Math.Sin( rotation ) * orbitRadius, transform.position.z );
+	}
+}
